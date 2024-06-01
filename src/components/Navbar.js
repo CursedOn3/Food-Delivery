46219ef2d90798a6b41358ef41c4becb4511f07a
Zuicyy <zuicy2021@gmail.com>
2024-06-01 10:23:42 +0545

    added option for order

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function Navbar() {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary navbar-dak bg-success">
+      <nav class="navbar navbar-expand-lg" style={{"background-color": "#9fd3c7"}}>
   <div className="container-fluid">
     <Link className="navbar-brand fs-1 fst-italic fst-bold" to="/">GoFood</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
