4fd2b0b2e0ebda4cc6573c8f6374896b1f56dc33
Zuicyy <zuicy2021@gmail.com>
2024-06-01 09:38:53 +0545

    understood React DOM

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -5,9 +5,15 @@ import Footer from '../components/Footer'
 export default function Home() {
   return (
     <div>
-      <div><Navbar/></div>
-      <p>This is home page</p>
-      <><Footer/></>
+      <div><Navbar /></div>
+      <div className="card mt-3" style={{ "width": "18rem", "maxHeight":"360px" }}>
+        <img src="..." className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+      <><Footer /></>
     </div>
   )
 }
