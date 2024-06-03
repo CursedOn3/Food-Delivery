db0d98907630d55ad4c50b9992554d7b2a2a202e
Zuicyy <zuicy2021@gmail.com>
2024-06-03 23:48:49 +0545

    made cards

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,33 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import Cards from '../components/Cards'
 
 export default function Home() {
   return (
     <div>
       <div><Navbar /></div>
-
-      <div className="card mt-3" style={{ "width": "18rem", "maxHeight": "360px", "background":"#a2a8d3" }}>
-        <img src="..." className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <div className='container'>
-            <select className='m-2 h-100 rounded'>
-              {Array.from(Array(6), (e, i) => {
-                return (
-                  <option key={i + 1} value={i + 1}>{i+1}</option>
-                )
-              })}
-            </select>
-            <select className='m-2 h-100 rounded'>
-              <option value="half">Half</option>
-              <option value="full">Full</option>
-            </select>
-          </div>
-        </div>
-      </div>
-      <><Footer /></>
+      <div><Cards /></div>
+      <div><Footer /></div>
     </div>
   )
 }
