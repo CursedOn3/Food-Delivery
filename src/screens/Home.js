4694b5878d5893259c4441ff3e5bac51612fcebd
Zuicyy <zuicy2021@gmail.com>
2024-06-04 00:15:58 +0545

    added crousel and login

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import Cards from '../components/Cards'
+import Craousel from '../components/Craousel'
 
 export default function Home() {
   return (
     <div>
       <div><Navbar /></div>
+      <div><Craousel/></div>
       <div><Cards /></div>
       <div><Footer /></div>
     </div>
